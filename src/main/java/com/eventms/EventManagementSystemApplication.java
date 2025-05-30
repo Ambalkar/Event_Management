d4f9ae24f52f50d6507c751e528d1647acac1102
Devendra Ambalkar <93560430+Ambalkar@users.noreply.github.com>
2025-05-30 18:19:11 +0530

    test 8

--- src/main/java/com/eventms/EventManagementSystemApplication.java
+++ src/main/java/com/eventms/EventManagementSystemApplication.java
@@ -2,15 +2,21 @@ package com.eventms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.eventms")
+@EntityScan("com.eventms.model")
+@EnableJpaRepositories("com.eventms.repository")
 public class EventManagementSystemApplication extends SpringBootServletInitializer {
 
     @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(EventManagementSystemApplication.class);
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(EventManagementSystemApplication.class);
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/com/eventms/repository/BaseRepository.java
@@ -0,0 +1,9 @@
+package com.eventms.repository;
+
+import com.eventms.model.BaseEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
+
+@NoRepositoryBean
+public interface BaseRepository<T extends BaseEntity> extends JpaRepository<T, Long> {
+} 
