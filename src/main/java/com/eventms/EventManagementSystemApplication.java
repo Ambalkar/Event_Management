623496ba1232b5e96ebc409f74dadff5a59af764
Devendra Ambalkar <93560430+Ambalkar@users.noreply.github.com>
2025-05-30 18:00:12 +0530

    test 6

---
+++ src/main/java/com/eventms/EventManagementSystemApplication.java
@@ -0,0 +1,19 @@
+package com.eventms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class EventManagementSystemApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(EventManagementSystemApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventManagementSystemApplication.class, args);
+    }
+} 
